e819bdd65e46f19155aa328b0360fb737e58b85b
sefcoskun35 <sefcoskun@gmail.com>
2026-07-15 22:53:33 +0000

    Enable Firestore persistent cache

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,17 +1,36 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
+import {
+  initializeFirestore,
+  persistentLocalCache,
+  persistentMultipleTabManager,
+} from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAN-Bkxe0tqZC3yZpQpIw_Bj0LQ5uQfGwU",
-  authDomain: "musteri-buddy.firebaseapp.com",
-  projectId: "musteri-buddy",
-  storageBucket: "musteri-buddy.firebasestorage.app",
-  messagingSenderId: "395650833082",
-  appId: "1:395650833082:web:1b92d7d887b7d928db0083",
-};
+  apiKey:
+    'AIzaSyAN-Bkxe0tqZC3yZpQpIw_Bj0LQ5uQfGwU',
+  authDomain:
+    'musteri-buddy.firebaseapp.com',
+  projectId: 'musteri-buddy',
+  storageBucket:
+    'musteri-buddy.firebasestorage.app',
+  messagingSenderId:
+    '395650833082',
+  appId:
+    '1:395650833082:web:1b92d7d887b7d928db0083',
+}
 
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(
+  firebaseConfig,
+)
 
-export const db = getFirestore(app);
-export const auth = getAuth(app);
+export const db =
+  initializeFirestore(app, {
+    localCache:
+      persistentLocalCache({
+        tabManager:
+          persistentMultipleTabManager(),
+      }),
+  })
+
+export const auth = getAuth(app)
